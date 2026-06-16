6f9dccdf430bfb5a688601abbeab081a08abf7fb
abilmansurq-rgb <abilmansur.q@icloud.com>
2026-06-16 20:14:59 +0500

    Initial commit

---
+++ src/client/java/com/beyondthedeep/client/BeyondTheDeepClient.java
@@ -0,0 +1,10 @@
+package com.beyondthedeep.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeyondTheDeepClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/beyondthedeep/client/BeyondTheDeepDataGenerator.java
@@ -0,0 +1,11 @@
+package com.beyondthedeep.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BeyondTheDeepDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
