6f218b84a336d2b9cb1c7b44a66c21a239e1d439
abilmansurq-rgb <abilmansur.q@icloud.com>
2026-06-18 01:43:52 +0500

    i.i.i
    temporary

--- src/main/java/com/beyondthedeep/event/ModEvents.java
+++ src/main/java/com/beyondthedeep/event/ModEvents.java
@@ -4,6 +4,13 @@ import com.beyondthedeep.items.custom.VoidRequiemItem;
 import net.fabricmc.fabric.api.entity.event.v1.ServerEntityCombatEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 // ... импорты
 
 public class ModEvents {
@@ -36,7 +43,38 @@ public class ModEvents {
         });
     }
 
-    private static void registerDeathEvents() {
-        // Сюда добавим логику штрафа 25% при смерти
+    public static void registerDeathEvents() {
+        ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> {
+            // Проходим по инвентарю
+            for (int i = 0; i < newPlayer.getInventory().size(); i++) {
+                var stack = newPlayer.getInventory().getStack(i);
+
+                if (stack.getItem() instanceof VoidRequiemItem) {
+                    var nbt = stack.getOrCreateNbt();
+                    float bonus = nbt.getFloat("DamageBonus");
+
+                    if (bonus > 0.05f) {
+                        // Уменьшаем на 20% (оставляем 80%)
+                        nbt.putFloat("DamageBonus", bonus * 0.8f);
+                        // 1. Добавляем звук «потухания» (звук разбитого стекла или магии)
+                        newPlayer.getWorld().playSound(null, newPlayer.getX(), newPlayer.getY(), newPlayer.getZ(),
+                                SoundEvents.BLOCK_GLASS_BREAK, SoundCategory.PLAYERS, 1.0f, 0.5f);
+
+                        ServerWorld serverWorld = (ServerWorld) newPlayer.getWorld();
+
+                        serverWorld.spawnParticles(
+                                ParticleTypes.SCULK_SOUL, // Тип частицы
+                                newPlayer.getX(), newPlayer.getY() + 1.0, newPlayer.getZ(), // Позиция
+                                20, // Количество
+                                0.5, 0.5, 0.5, // Разброс (delta)
+                                0.1 // Скорость
+                        );;
+                        newPlayer.sendMessage(Text.translatable("message.beyondthedeep.void_echo").formatted(Formatting.RED), true);
+                    } else {
+                        nbt.putFloat("DamageBonus", 0.0f);
+                    }
+                }
+            }
+        });
     }
 }
